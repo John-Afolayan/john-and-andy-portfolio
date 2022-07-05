537c69a667ce7eb6dda2ced5539375f2045f1633
John-Afolayan <49284081+John-Afolayan@users.noreply.github.com>
2022-07-05 00:59:55 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,5 @@
-tmux kill-server
 cd johns-portfolio-mlh
 git fetch && git reset origin/main --hard
-dnf install tmux
-tmux new
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install --upgrade pip
